e18411ae2dd372e1c0bb6eb7199c99c0106fad6a
Ewelina <ekowalczyk1994@gmail.com>
2022-11-20 17:50:05 -0600

    header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import NavBar from './NavBar';
+import Header from './Header';
 
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Header/>
      
     </div>
   );
