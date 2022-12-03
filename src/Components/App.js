2898eff40f2914a1a2b6955358ea79be5821c350
Ewelina <ekowalczyk1994@gmail.com>
2022-12-03 11:29:33 -0600

    about section

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import NavBar from './NavBar';
 import Header from './Header';
-
+import About from './About';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Header/>
-     
+     <About/>
     </div>
   );
 }
