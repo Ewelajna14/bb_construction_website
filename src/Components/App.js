88e66d0b6c7594611b71ef4425c696191e92f21f
Ewelina <ekowalczyk1994@gmail.com>
2022-12-29 15:02:21 -0600

    services

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NavBar from './NavBar';
 import Header from './Header';
 import About from './About';
-import Gallery from './Gallery';
+import Services from './Services';
 import Form from './Form';
 import Footer from './Footer';
 
@@ -13,7 +13,7 @@ function App() {
       <NavBar/>
       <Header/>
      <About/>
-     <Gallery/>
+     <Services/>
      <Form/>
      <Footer/>
     </div>
